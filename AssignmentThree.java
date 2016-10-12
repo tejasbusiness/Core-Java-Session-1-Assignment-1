e88fc89d78546306f6da1fc87d64d78a0bc79c32
tejasbusiness <tejasbusiness@gmail.com>
2016-10-12 22:59:04 +0530

    Add files via upload

---
+++ AssignmentThree.java
@@ -0,0 +1,18 @@
+package pkgAsignmnt;
+
+public class AssignmentThree {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		
+		b-=a;
+		System.out.println( "Value of b is " + b );
+		
+		a+=b;
+		System.out.println( "Value of a is " + a );
+
+	}
+
+}
