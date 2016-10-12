4fb31a08cca0cbc7085da71ccfee4f74cbb5be2f
tejasbusiness <tejasbusiness@gmail.com>
2016-10-12 22:53:36 +0530

    Add files via upload

---
+++ AssignmentTwo.java
@@ -0,0 +1,22 @@
+package pkgAsignmnt;
+
+public class AssignmentTwo {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		
+		int b=a-- - --a;
+		System.out.println( "Value of b is " + b );
+		
+		int c=a--;
+		System.out.println( "Value of c is " + c );
+		
+		int d=a>>2;
+		System.out.println( "Value of d is " + d);
+		
+		int e=a&b;
+		System.out.println( "Value of e is " + e);
+	}
+
+}
