f44c99554d4ab682dc66772005af5d117ae01fce
tejasbusiness <tejasbusiness@gmail.com>
2016-10-12 23:09:02 +0530

    Add files via upload

---
+++ AssignmentFour.java
@@ -0,0 +1,13 @@
+package pkgAsignmnt;
+
+public class AssignmentFour {
+
+	public static void main(String[] args) {
+		
+		for ( int i = 65; i <= 90; i++ ) {
+			char character = ( char ) i;
+			System.out.println( character + " - " + i );
+		}
+	}
+
+}
