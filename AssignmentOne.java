b64b225faeb0438552a48073aabd3f9db54278f0
tejasbusiness <tejasbusiness@gmail.com>
2016-10-12 22:39:22 +0530

    Add files via upload

---
+++ AssignmentOne.java
@@ -0,0 +1,15 @@
+package pkgAsignmnt;
+
+public class AssignmentOne {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		b +=a;
+		
+		System.out.println( "a + b = " + b );
+
+	}
+
+}
